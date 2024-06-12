c3895c303ca4b868ba423ddbd993aa5d332e411f
3Lsome <luckyllemos@gmail.com>
2024-06-12 21:38:49 +0800

    Update update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -22,19 +22,19 @@ import {
 const router = express.Router();
 
 router.get("/individuals", showAllIndividual);
+router.get("/roles", showAllRole);
 
 router.post("/get-individual-by-email", getIndividualByEmail);
-
-router.put("/update-individual-by-email/:email/:indId", updateIndividualByEmail);
-
-router.delete("/del-ind-by-email-and-id/:email/:indId", deleteIndividualByEmail);
-
 router.post("/individual", createIndividual);
-
 router.post("/login-credentials", loginCredentials);
 router.post("/check-email-exist", checkEmailExist);
 router.post("/call-recordings", getCallRecording);
-router.get("/roles", showAllRole);
+
+router.put("/update-individual-by-email/:email/:indId", updateIndividualByEmail);
+
+
+router.delete("/del-ind-by-email-and-id/:email/:indId", deleteIndividualByEmail);
+
 
 
 export default router;
